d67de8f7a840fe0c4d79b7afeee934324816380b
kouros <kouros@example.com>
2019-03-24 22:09:38 +0600

    express server v0.1
    express server v0.1

---
+++ client/build/precache-manifest.7b2b8b2cfe1049e7c163f7b13b8f007e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b83b540f5401efb75a96",
+    "url": "/static/css/main.a893143a.chunk.css"
+  },
+  {
+    "revision": "b83b540f5401efb75a96",
+    "url": "/static/js/main.99bb2fcf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "631d3e2a1b320ee0fbf9",
+    "url": "/static/css/2.ed719924.chunk.css"
+  },
+  {
+    "revision": "631d3e2a1b320ee0fbf9",
+    "url": "/static/js/2.91231d85.chunk.js"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "f261aadf441df064026eaa81f5d3c13c",
+    "url": "/index.html"
+  }
+];
